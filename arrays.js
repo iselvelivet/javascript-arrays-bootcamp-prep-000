946b63c7eda182e4b3547c09d150287ffecc6909
Tim Sander <tomzander88@yahoo.com>
2017-11-12 15:47:34 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = new Array();
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+function addElementToBeginningOfArray (array, element) {
+  array.push(element)
+}
+
