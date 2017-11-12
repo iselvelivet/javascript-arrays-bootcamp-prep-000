1888ad944631f406dbc9501b8f97c8aab824ccad
Tim Sander <tomzander88@yahoo.com>
2017-11-12 15:48:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = new Array();
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray (array, element) {
-  array.push(element)
+  var array = new Array()
+  array.push(element);
 }
